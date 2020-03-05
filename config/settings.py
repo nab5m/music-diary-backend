bcd078d934fc625a76134137417ea808634fbb48
nab5m <sk990420@naver.com>
2020-03-05 11:28:55 +0900

    add cors-whitelist

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,8 @@ import os
 from config import secret_settings
 
 CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
 )
